79abcafad73adff206f4a8a455863099be439289
Chris Gunther <chris@chrisgunther.net>
2010-12-13 23:14:52 -0500

    lots more work on POAs

--- lib/lightning-source/purchase_order_acknowledgment.rb
+++ lib/lightning-source/purchase_order_acknowledgment.rb
@@ -1,22 +1,114 @@
 module LightningSource
   class PurchaseOrderAcknowledgment
     
+    attr_accessor :client_identification, :batch_id, :batch_date, :batch_time
+    
+    Order = Struct.new(:client_order_number, :client_order_date, :errors) do
+      def items
+        @items ||= []
+      end
+    end
+    
+    # TODO: Add helper methods like #accepted?, #cancelled?, etc.
+    Item = Struct.new(:line_item_number, :title_id, :order_quantity, :title_id_13, :status_code, :ship_quantity, :error)
+    
     def self.fetch(opts)
       require 'net/ftp'
       require 'tempfile'
       
       ftp = Net::FTP.new(opts[:host], opts[:username], opts[:password])
       ftp.chdir('outgoing')
-      ftp.list('*.PPR').compact.map do |file|
+      result = ftp.list('*.PPR').compact.map do |file|
         filename = file.split[-1]
         tempfile = Tempfile.new(filename)
         ftp.gettextfile(filename, tempfile.path)
         PurchaseOrderAcknowledgment.new(tempfile)
       end
+      ftp.close
+      result
     end
     
     def initialize(raw_data)
       @raw_data = raw_data.read
+      
+      expected_data_record_count = parse_numeric(@raw_data.lines.to_a[-1][35..41])
+      actual_data_record_count = @raw_data.lines.to_a.size - 2
+      
+      if @raw_data.lines.to_a[0] !~ /^\$\$HDR/ || @raw_data.lines.to_a[-1] !~ /^\S\SEOF/ ||
+        expected_data_record_count != actual_data_record_count
+        raise 'malformed file'
+      end
+      
+      @raw_data.each_line do |line|
+        if line[0..4] == '$$HDR'
+          parse_batch_header line
+        elsif line[0..1] == 'H1'
+          # TODO: Test multiple orders in a single POA
+          parse_header line
+        elsif line[0..1] == 'H2'
+          # TODO: Handle POA Header Error Message
+        elsif line[0..1] == 'D1'
+          parse_line_item line
+        elsif line[0..1] == 'D2'
+          parse_line_item_error_message line
+        end
+      end
+    end
+    
+    def orders
+      @orders ||= []
+    end
+    
+  private
+  
+    def parse_numeric(string)
+      string.to_i
+    end
+    
+    def parse_date(string)
+      Date.parse(string)
+    end
+    
+    def parse_time(string)
+      string.gsub(/(\d{2})(\d{2})(\d{2})/, '\1:\2:\3')
+    end
+    
+    def parse_string(string)
+      string.strip
+    end
+  
+    def parse_batch_header(raw_data)
+      self.client_identification = raw_data[5..10]
+      self.batch_id = parse_numeric(raw_data[11..20])
+      self.batch_date = parse_date(raw_data[21..28])
+      self.batch_time = parse_time(raw_data[29..34])
+    end
+    
+    def parse_header(raw_data)
+      order = Order.new
+      order.client_order_number = parse_string(raw_data[12..26])
+      order.client_order_date = parse_date(raw_data[27..34])
+      orders << order
+    end
+    
+    def parse_line_item(raw_data)
+      client_order_number = parse_string(raw_data[12..26])
+      item = Item.new
+      item.line_item_number = parse_numeric(raw_data[27..31])
+      item.title_id = raw_data[32..41]
+      item.order_quantity = parse_numeric(raw_data[42..50])
+      item.title_id_13 = parse_string(raw_data[51..70])
+      orders.find { |o| o.client_order_number == client_order_number }.items << item
+    end
+    
+    def parse_line_item_error_message(raw_data)
+      client_order_number = parse_string(raw_data[12..26])
+      line_item_number = parse_numeric(raw_data[27..31])
+      order = orders.find { |o| o.client_order_number == client_order_number }
+      item = order.items.find { |i| i.line_item_number == line_item_number }
+      
+      item.status_code = raw_data[42..43]
+      item.ship_quantity = parse_numeric(raw_data[44..52])
     end
     
   end
--- test/test_purchase_order_acknowledgment.rb
+++ test/test_purchase_order_acknowledgment.rb
@@ -10,6 +10,7 @@ class TestPurchaseOrderAcknowledgment < Test::Unit::TestCase
       ftp = mock 'ftp' do
         expects(:chdir).with('outgoing')
         expects(:list).with('*.PPR').returns([nil])
+        expects(:close)
       end
       Net::FTP.expects(:new).returns(ftp)
       
@@ -22,8 +23,10 @@ class TestPurchaseOrderAcknowledgment < Test::Unit::TestCase
           expects(:chdir).with('outgoing')
           expects(:list).with('*.PPR').returns(['-rw-r--r--    1 501      482             4 Dec 13 14:42 sample_poa.PPR'])
           expects(:gettextfile)
+          expects(:close)
         end
         Net::FTP.expects(:new).returns(ftp)
+        Tempfile.expects(:new).returns(File.open(File.join(File.dirname(File.expand_path(__FILE__)), 'fixtures/M121314096.PPR')))
       end
       
       should "return all POAs" do
@@ -45,6 +48,77 @@ class TestPurchaseOrderAcknowledgment < Test::Unit::TestCase
       assert @poa
     end
     
+    should "set the client_identification" do
+      assert_equal 'OFFBKS', @poa.client_identification
+    end
+    
+    should "set the batch_id" do
+      assert_equal 157, @poa.batch_id
+    end
+    
+    should "set the batch_date" do
+      assert_equal Date.parse('2010-12-13'), @poa.batch_date
+    end
+    
+    should "set the batch_time" do
+      assert_equal '14:09:27', @poa.batch_time
+    end
+    
+    context "orders" do
+      setup { @order = @poa.orders.first}
+      
+      should "set the orders" do
+        assert_equal 1, @poa.orders.size
+      end
+
+      should "set the client_order_number" do
+        assert_equal '954', @order.client_order_number
+      end
+
+      should "set the client_order_date" do
+        assert_equal Date.parse('2010-12-13'), @order.client_order_date
+      end
+
+      should "not set any errors" do
+        assert_nil @order.errors
+      end
+      
+      context "items" do
+        setup { @item = @order.items.first }
+        
+        should "set the order's items" do
+          assert_equal 1, @poa.orders.first.items.size
+        end
+
+        should "set the line_item_number" do
+          assert_equal 947, @item.line_item_number
+        end
+
+        should "set the title_id" do
+          assert_equal '0982771614', @item.title_id
+        end
+
+        should "set the order_quantity" do
+          assert_equal 1, @item.order_quantity
+        end
+
+        should "set the title_id_13" do
+          assert_equal '9780982771617', @item.title_id_13
+        end
+        
+        should "set the status_code" do
+          assert_equal 'AR', @item.status_code
+        end
+        
+        should "set the ship_quantity" do
+          assert_equal 1, @item.ship_quantity
+        end
+        
+        should "not set an error" do
+          assert_nil @item.error
+        end
+      end
+    end
   end
   
 end
